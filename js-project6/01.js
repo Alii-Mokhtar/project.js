60ce8f56e2ac77eb5566d60631256cff2a0a1e3c
Alii-Mokhtar <158050049+Alii-Mokhtar@users.noreply.github.com>
2024-03-02 16:35:57 +0330

    Add files via upload

---
+++ js-project6/01.js
@@ -0,0 +1,25 @@
+
+
+sum = ""
+for (i = 0; i < 6; i++) {
+    let a = Math.floor(Math.random() * 57 + 65)
+    let z = String.fromCharCode(a)
+    sum += z
+
+}
+console.log(sum)
+
+
+
+
+
+// function create(){
+// let random = ''
+// let char = 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+// for(let i= 0;i<6;i++){
+//     random+=char.charAt(Math.floor(Math.random()*char.length))
+// }
+// console.log(random)
+// }
+// create()
+
